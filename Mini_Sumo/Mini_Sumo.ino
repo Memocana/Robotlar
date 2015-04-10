15897ff8f7d11b8599eda42818f598a9f24ce41b
Memocana <Memo@Memows-MacBook-Air.local>
2015-04-10 12:16:10 +0200

    sensor'lü

--- Mini_Sumo/Mini_Sumo.ino
+++ Mini_Sumo/Mini_Sumo.ino
@@ -1,13 +1,13 @@
-int  motor1A=A1;
-int  motor1B=A0;
+int  motor1A=A0;
+int  motor1B=A1;
 int  motor1E=5;
-int  motor2A=10;
-int  motor2B=9;
+int  motor2A=9;
+int  motor2B=10;
 int  motor2E=11;
 int  sensorF=8;
-int  sensorR=6;
-int  sensorL=7;
-int  button=A2;
+int  sensorR=7;
+int  sensorL=6;
+int  button1=A2;
 int  led1=0;
 int  led2=1;
 int  lastSensor = 0;
@@ -24,13 +24,14 @@ void setup() {
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(sensorF, INPUT);
-  pinMode(sensorR, INPUT);
-  pinMode(sensorL, INPUT);
-  pinMode(qtrL, INPUT);
-  pinMode(qtrR, INPUT);
-  pinMode(pot1, INPUT);
-  pinMode(pot2, INPUT);
-  pinMode(pot3, INPUT);
+  pinMode(sensorR, INPUT_PULLUP);
+  pinMode(sensorL, INPUT_PULLUP);
+  pinMode(button1, INPUT_PULLUP);
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);  
+  delay(1000);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);  
 }
 
 void movement(int leftMotor, int rightMotor) {
@@ -46,14 +47,14 @@ void movement(int leftMotor, int rightMotor) {
   }
   //leftMotor E'yi buraya mı alsam?
   if (rightMotor < 0) {
-    digitalWrite(motor1A,LOW);
-    digitalWrite(motor1B,HIGH);
+    digitalWrite(motor2A,LOW);
+    digitalWrite(motor2B,HIGH);
   } else if (rightMotor == 0) {
-    digitalWrite(motor1A,LOW);
-    digitalWrite(motor1B,LOW);
+    digitalWrite(motor2A,LOW);
+    digitalWrite(motor2B,LOW);
   } else {
-    digitalWrite(motor1A,HIGH);
-    digitalWrite(motor1B,LOW);
+    digitalWrite(motor2A,HIGH);
+    digitalWrite(motor2B,LOW);
   }
   powerMotor(motor1E, abs(leftMotor));
   powerMotor(motor2E, abs(rightMotor));
@@ -71,7 +72,7 @@ void forward(int leftMotor, int rightMotor){
 void backward(int leftMotor, int rightMotor){
   movement(-leftMotor, -rightMotor);
 }
-void leftOneWheel(int leftMotor, int rightMotor){
+void leftOneWheel(int leftMotor){
   movement(leftMotor,0);
 }
 void rightOneWheel(int rightMotor){
@@ -86,7 +87,24 @@ void rightReverse(int leftMotor, int rightMotor){
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(led1, digitalRead(sensorF));
+  if(!digitalRead(sensorR)) {
+    forward(100, 100);
+    delay(10);
+  } else if(!digitalRead(sensorL)) {
+    leftOneWheel(100);
+    delay(10);
+  }  else {
+    stopper();
+    delay(10);
+  }
+ /*
+  if(digitalRead(sensorF)) {
+    forward(100,100);
+    delay(10);
+  } else if(!digitalRead(sensorL)) {
+    leftOneWheel(100);
+    delay(10);
+  }*/
 }
 
 
